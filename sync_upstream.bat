0066b00e4010003a1f5b624610508226374f2fbe
mheene <markus.heene@gmail.com>
2019-12-23 11:03:46 +0100

    Merged upstream

--- sync_upstream.bat
+++ sync_upstream.bat
@@ -2,5 +2,8 @@ git pull origin
 git fetch upstream
 git checkout master
 git merge upstream/master
+echo %ERRORLEVEL%
 git commit -m "Merged upstream" . 
+echo %ERRORLEVEL%
 git push origin master
+echo %ERRORLEVEL%
